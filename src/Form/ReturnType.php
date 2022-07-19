fb1efa9349ddefdae343c1dec130af9fc8990e21
Andjela <andjaaa95@gmail.com>
2022-07-19 15:51:46 +0200

    prepairing for insert

--- src/Form/ReturnType.php
+++ src/Form/ReturnType.php
@@ -6,6 +6,7 @@ use App\Entity\Users;
 use App\Entity\Status;
 use App\Entity\Country;
 use App\Entity\Returns;
+use App\Entity\ReturnImages;
 use App\Entity\ReasonSettings;
 use App\Entity\ResellerAddress;
 use App\Entity\ResellerShipments;
@@ -17,16 +18,19 @@ use Symfony\Component\Form\AbstractType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\HttpFoundation\RequestStack;
 use App\Repository\ResellerShipmentItemsRepository;
+use App\Repository\ReturnsRepository;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ReturnType extends AbstractType
 {
@@ -35,9 +39,11 @@ class ReturnType extends AbstractType
     public $resellerAddress;
     public $order;
     public $shipmentsItems;
-    
+    public $returnsRepository;
+    public $orderId;
+    public $userEmail;
 
-    public function __construct(RequestStack $requestStack, ManagerRegistry $doctrine) {
+    public function __construct(RequestStack $requestStack, ManagerRegistry $doctrine,ReturnsRepository $returnsRepository) {
         $this->requestStack = $requestStack;
         $this->doctrine = $doctrine;
         
@@ -49,13 +55,11 @@ class ReturnType extends AbstractType
         $this->resellerAddress = $this->doctrine->getRepository(ResellerAddress::class)->findOneBy(['id'=>$this->order->getDeliveryAddressId()]);
         $this->shipmentsItems = $this->doctrine->getRepository(ResellerShipmentItems::class)->findBy(['shipment_id'=> $this->order->getId()]);
         
-        
-
+        $this->orderId = $webshopOrderId;
+        $this->userEmail = $email;
     }   
 
 
-    
-    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('order_id', TextType::class, [
@@ -120,7 +124,7 @@ class ReturnType extends AbstractType
             ->add('reference', TextType::class, [
                 'required' => 'References is required field',
                 'attr' => ['class' => 'form-control', 'disabled'=>'disabled'],
-                'data' => $this->order->getReference()
+                'data' =>  $this->order->getReference()
             ])
             ->add('client_name', TextType::class, [
                 'required' => 'Order id is required field',
@@ -134,12 +138,12 @@ class ReturnType extends AbstractType
             ])
            
             ->add('company_name', TextType::class, [
-                'required' => 'Order id is required field',
+                'required' => 'Company name is required field',
                 'attr' => ['class' => 'form-control'],
                 'data' => $this->resellerAddress->getCompanyName()
             ])
             ->add('street', TextType::class, [
-                'required' => 'Order id is required field',
+                'required' => 'Strret is required field',
                 'attr' => ['class' => 'form-control'],
                 'data' => $this->resellerAddress->getStreet()
             ])
@@ -151,35 +155,89 @@ class ReturnType extends AbstractType
           
             ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
 
-                // $form = $event->getForm();
-                
-                // $request = $this->requestStack->getCurrentRequest();
-                // $all = $request->request->all();
-                
-                // $email = $all['search_return']['user_email'];
-                // // return dd($request);
-                // $webshopOrderId = $all['search_return']['webshop_order_id'];
-                // $order = $this->doctrine->getRepository(ResellerShipments::class)->findOneBy(['webshopOrderId'=>$webshopOrderId]);
-                
-                // if(!$order)
-                // {
-                //     $form->get('webshop_order_id')->addError(new FormError('There is no order with id: '.$webshopOrderId));              
-                // }
-                // else
-                // {
-                //     $customerId = $order->getCustomerId();
-                    
-                //     $customer = $this->doctrine->getRepository(Users::class)->findOneBy(['id' => $customerId]);
-                    
-                //     $customeremail = $customer->getUsername();
-                    
+                $request = $this->requestStack->getCurrentRequest();
+                $form = $event->getForm();
+                $this->returnsRepository = new Returns();
 
-                //     if($email != $customeremail)
-                //     {
-                //         $form->get('user_email')->addError(new FormError('There user with email : '.$email));              
-                //     }
+               
+                $all =  $all = $request->request->all();
+                return dd($all);
+                $shablon = $all['return']['shablon'];
 
-                // }
+                //find status
+                $statusId = $all['return']['status'];
+                $status = $this->doctrine->getRepository(Status::class)->findOneBy(['id'=>$statusId]);
+               
+                $clientEmail = $all['return']['client_email'];
+                $clientName = $all['return']['client_name'];
+                $companyName = $all['return']['company_name'];
+                $street = $all['return']['street'];
+                $postalCode = $all['return']['postal_code'];
+
+                $this->returnsRepository->setReference($this->order->getReference());
+                $this->returnsRepository->setWebshopOrderId($this->orderId);
+                $this->returnsRepository->setStatus($status);
+                $this->returnsRepository->setUserEmail($this->userEmail);
+                $this->returnsRepository->setClientEmail($clientEmail);
+                $this->returnsRepository->setClientName($clientName);
+                $this->returnsRepository->setCompanyName($companyName);
+                $this->returnsRepository->setStreet($street);
+                $this->returnsRepository->setPostCode($postalCode);
+                
+                if($shablon == "admin-r")
+                {
+                    $reasonId = $all['return']['reasons'];
+                    $reasons = $this->doctrine->getRepository(ReasonSettings::class)->findOneBy(['id'=>$reasonId]);
+                    
+                    $this->returnsRepository->setReason($reasons->getName());
+                }
+                else
+                {
+                    $reasonUser = $request->request->get('reason-user');
+                    
+                    $photos = $request->request->all('photos');
+                    
+                    if($photos)
+                    {
+                        foreach ($photos as $photo) {
+
+
+                            $savephoto = new ReturnImages();
+                            $savephoto->setReturnId($this->returnsRepository->getId());
+                            
+                            $savephoto->setCreatedAt(new \DateTime());
+                            $entityManagerPhoto = $this->doctrine->getManager();
+                            $entityManagerPhoto->persist($savephoto); 
+        
+                            
+                            $originalname = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+                            $sname = $this->slugger->slug($originalname);
+            
+                            $newName = $sname.'-'.uniqid().'.'.$photo->guessExtension();
+                        
+                                $photo->move(
+                                    $this->getParameter('return_images'),
+                                    $newName
+                                );
+                        
+                            $savephoto->setUrl($newName);
+                        }
+        
+                        try{
+                            $entityManagerPhoto->flush();
+                            
+                        }
+                        catch (FileException $e) {
+
+                            $contents = $this->renderView('errors/500.html.twig', []);
+                    
+                            return new Response($contents, 500);
+                        }
+                    }
+                }
+
+             
+         
                 
 
             })
@@ -189,7 +247,7 @@ class ReturnType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Status::class,
+            'data_class' => null,
         ]);
     }
 }
