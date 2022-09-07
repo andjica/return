92e574138ce8e433071d9aa7038d828a91a31651
Andjela Stojanovic <ucps.inf@gmail.com>
2022-09-07 11:53:16 +0200

    check update

--- src/Controller/ShippingSettingsController.php
+++ src/Controller/ShippingSettingsController.php
@@ -31,31 +31,39 @@ class ShippingSettingsController extends AbstractController
   
     }
     
-     /**
-     * @Route("/settings/shipping", methods={"GET", "POST"}, name="settings_shipping")
+    /**
+     * @Route("/settings/shipping", defaults={"_country": "", "_distributor": ""}, methods={"GET", "POST"}, name="settings_shipping_prices")
+     * @Route("/settings/shipping/{_country}", defaults={"_distributor": ""}, methods={"GET", "POST"}, name="settings_shipping_prices_country")
+     * @Route("/settings/shipping/{_country}/{_distributor}", methods={"GET", "POST"}, name="settings_shipping_prices_country_distributor")
      */
-    public function index(ShippingPriceRepository $shipping_prices, CountryRepository $countries, string $_country): Response
+    public function index(ShippingPriceRepository $shipping_prices, CountryRepository $countries, string $_country, string $_distributor): Response
     {
 
         
         $customer = $this->doctrine->getRepository(Customer::class)->findOneBy(['isReseller' => true]);
         $country = $countries->findOneBy(['iso_code' => $_country]);
-          
         
+        return dd($customer->getCurrentShippingPriceHistory());
+
         $countries_items = [];
         $shippingpriceHistory = $this->doctrine->getRepository(ShippingPriceHistory::class)->findOneBy(['customer'=>$customer->getId()]);
-      
+       
+        // return dd($shippingpriceHistory);
 
         foreach ($shipping_prices->countries($customer->getCurrentShippingPriceHistory()) as $s_price) {
 
+            
             $countr = $s_price->getCountry();
+            // return dd($countr);
+            // $shippingOption = $shippingPrice->getShippingOption();
 
+            // echo $shippingOption->getName();
             if (!isset($countries_items[$countr->getId()])) {
 
                 $countries_items[$countr->getId()] = [
                     'group' => $countr->getIsEu() ? 'EU' : 'label.rest_of_the_world',
                     'code' => $countr->getISOCode(),
-                    'name' => 'Andjca',
+                    'name' => $countr->getName()
                    
                 ];
 
