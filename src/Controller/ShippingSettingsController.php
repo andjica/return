9fc4b3741c3abbb03f5433cf3bef3029c600bc39
Andjela Stojanovic <ucps.inf@gmail.com>
2022-09-08 17:49:27 +0200

    new design and shipping options

--- src/Controller/ShippingSettingsController.php
+++ src/Controller/ShippingSettingsController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Common\Country;
 use App\Entity\Returns\Status;
+use App\Entity\Common\CostPrice;
 use App\Entity\Reseller\Customer;
 use App\Entity\Returns\PayCategory;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\Common\CountryRepository;
 use App\Entity\Reseller\ShippingPriceHistory;
+use App\Repository\Common\CostPriceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\Reseller\ShippingPriceRepository;
@@ -38,7 +40,7 @@ class ShippingSettingsController extends AbstractController
      * @Route("/settings/shipping/{_country}", defaults={"_distributor": ""}, methods={"GET", "POST"}, name="settings_shipping_prices_country")
      * @Route("/settings/shipping/{_country}/{_distributor}", methods={"GET", "POST"}, name="settings_shipping_prices_country_distributor")
      */
-    public function index(ShippingPriceRepository $shipping_prices, CountryRepository $countries, string $_country, string $_distributor): Response
+    public function index(ShippingPriceRepository $shipping_prices, CountryRepository $countries, string $_country, string $_distributor, CostPriceRepository $cost_prices): Response
     {
 
         
@@ -53,7 +55,25 @@ class ShippingSettingsController extends AbstractController
         }
         $countries_items = [];
         $shippingpriceHistory = $this->doctrine->getRepository(ShippingPriceHistory::class)->findOneBy(['customer'=>$customer->getId()]);
-       
+
+        //get countries
+        foreach ($shipping_prices->countries($customer->getCurrentShippingPriceHistory()) as $s_price) {
+
+            $c = $s_price->getCountry();
+
+            if (!isset($countries_items[$c->getId()])) {
+
+                $countries_items[$c->getId()] = [
+                    'group' => $c->getIsEu() ? 'EU' : 'label.rest_of_the_world',
+                    'code' => $c->getISOCode(),
+                    'name' => $c->getName(),
+                ];
+
+            }
+
+        }
+
+        //get all distributors
         $distributors_items = [];
         
         foreach ($shipping_prices->distributors($customer->getCurrentShippingPriceHistory(), $country) as $distrib) {
@@ -66,26 +86,37 @@ class ShippingSettingsController extends AbstractController
 
         }
         
-        $shippingOptions = [];
-
-
+        //get shipping options
         foreach ($shipping_prices->shippingOptions($customer->getCurrentShippingPriceHistory(),  $country, $distrib) as $shoption) {
+           
+            $shippingOption = $shoption->getShippingOption();
+            $shippingOptionId = $shippingOption->getId();
+        
+            $distributor =  $shippingOption->getDistributor();
+            $is_colli = $shippingOption->getIsColli();
 
-            return  dd( $shoption->getShippingOption());
-
-            
-
+          
         }
-        
-      
 
-        
+        //get selected distributors
         $distributors_selected = [];
         $options_items = [];
+        foreach ($shipping_prices->distributors($customer->getCurrentShippingPriceHistory(), $country, $_distributor) as $distrib) {
 
-       
-
-        
+            $distributors_selected[] =
+            [
+                'id' => $distrib->getId(),
+                'name' => $distrib->getName(),
+                'key_name' => $distrib->getKeyName(),
+            ];
+            if (!isset($options_items[$distrib->getId()])) {
+                $options_items[$distrib->getId()] = [];
+            }
+            $options_items[$distrib->getId()][] = [
+                'id' =>  $shippingOption->getId(),
+                'name' =>  $shippingOption->getName()
+            ];
+        }
         
 
 
@@ -94,6 +125,10 @@ class ShippingSettingsController extends AbstractController
             'payments' => $this->payments,
             'countries' => $countries_items,
             'country_selected' => $country,
+            'distributors' => $distributors_items,
+            'distributor_selected' => $_distributor,
+            'distributors_selected' => $distributors_selected,
+            'options' => $options_items
         ]);
     }
 
